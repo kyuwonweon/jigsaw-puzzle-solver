0f628f0645e6314c29786ca32b778f6e8e5f72eb
kyuwon <kyuwon0917@gmail.com>
2026-05-21 21:45:38 -0500

    gaussian smoothing

--- cv/edge_detection.py
+++ cv/edge_detection.py
@@ -7,8 +7,28 @@ img = Image.open("image/puzzle_template.jpg").convert("L")
 I = np.array(img, dtype=float)
 
 # Gaussian Smoothing
+def gaussian_kernel(size, sigma):
+    k = size // 2
+    y,x = np.mgrid[-k:k+1, -k:k+1]
+    G = np.exp(-(x**2 + y**2)/(2*sigma**2))
+    G_norm = G/G.sum()
+    return G_norm
 
-# Image Gradient 
+def conv2d(img, kernel):
+    k = kernel.shape[0]//2
+    padded = np.pad(img, k, mode ="reflect")
+    output = np.zeros(img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):     
+            patch = padded[i:i+kernel.shape[0],
+                           j:j+kernel.shape[1]]
+            output[i,j] = np.sum(patch*kernel)
+    return output
+
+k = gaussian_kernel(size = 5, sigma = 2)
+S = conv2d(I, k)
+
+# Image Gradient
 
 # Non-maxima Suppression
 
@@ -17,7 +37,7 @@ I = np.array(img, dtype=float)
 # Edge linking 
 
 # Result Visualization 
-plt.imshow(I, cmap='gray')
+plt.imshow(S, cmap='gray')
 plt.title('Loaded Grayscale Image')
 plt.axis('off')
 plt.show()
